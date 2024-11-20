ac5619e843613166e54fe99c81485701a675d5e0
3akare <bakaredavid007@gmail.com>
2024-11-20 17:06:51 +0100

    feat: client UI, and landmark collection (background thread)

---
+++ version-4/client.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template
+import cv2
+import mediapipe as mp
+import numpy as np
+import pickle
+import threading
+
+app = Flask(__name__)
+
+model_dict = pickle.load(open('models/model.p', 'rb'))
+model = model_dict['model']
+
+webcam_running = True
+
+mp_hands = mp.solutions.hands # mediapipe hands solution
+hands = mp_hands.Hands(
+    static_image_mode=False,  # Set to False for real-time video tracking
+    min_detection_confidence=0.3,  # Confidence threshold for hand detection
+    min_tracking_confidence=0.5   # Confidence threshold for maintaining hand tracking
+)
+
+def capture_images():
+    """Background function to capture images from the webcam."""
+    global webcam_running
+    cap = cv2.VideoCapture(0)
+
+    while webcam_running:
+        ret, frame = cap.read() # ret = capture status (boolean)
+        frame_h, frame_w, _ = frame.shape
+        hands._image_dimensions = (frame_w, frame_h) # Set Image Dimensions for MediaPipe
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Converts the frame from BGR (OpenCV's default color format) to RGB (required by MediaPipe)
+        normalized_coordinates = []
+        temp_x = []
+        temp_y = []
+
+        # process captured hands
+        results = hands.process(frame_rgb)
+        if not ret:
+            continue
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    # Extract and append landmarks
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    temp_x.append(x)
+                    temp_y.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    # Normalize Coordinates
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    normalized_coordinates.append(x - min(temp_x))
+                    normalized_coordinates.append(y - min(temp_y))
+
+                try:
+                    print(model.predict([np.asarray(normalized_coordinates)]))
+                except Exception as e:
+                    print("Error " + e)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    # run webcam in background thread
+    webcam_thread = threading.Thread(target=capture_images, daemon=True)
+    webcam_thread.start()
+    try:
+        app.run(debug=True)
+    finally:
+        webcam_running = False
+        webcam_thread.join()
